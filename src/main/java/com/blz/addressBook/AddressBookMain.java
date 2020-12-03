46709d0e0eaccf9c3327e619bea6dc1391655758
Your Name <you@example.com>
2020-12-03 20:27:52 +0530

    Refactored: code with multiple scanner objects to work properly

--- src/main/java/com/blz/addressBook/AddressBookMain.java
+++ src/main/java/com/blz/addressBook/AddressBookMain.java
@@ -1,18 +1,62 @@
 package com.blz.addressBook;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBookMain {
 
 	static AddressBookService addressBook = new AddressBookService();
+	static Map<String, AddressBookMain> addressBookObj = new HashMap();
+	static AddressBookMain addressobj = new AddressBookMain();
+	static Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	public static void addAddressBook() {
+		Scanner input = new Scanner(System.in);
+		System.out.println(
+				"Enter choice \n1.Create new addressbook " + "\n2.Add contacts into existing address Book \n3.Exit ");
+		int entry = input.nextInt();
+		if (entry != 3) {
+			switch (entry) {
+			case 1:
+				Scanner nameInput = new Scanner(System.in);
+				System.out.println("Enter name of new address book: ");
+				String nameOfNewBook = nameInput.nextLine();
+				if (addressBookObj.containsKey(nameOfNewBook)) {
+					System.out.println("Address book already exists");
+					break;
+				}
+				addressBookObj.put(nameOfNewBook, addressobj);
+				System.out.println("Address book " + nameOfNewBook + " has been added");
+				AddressBookMain.entries();
+				break;
+			case 2:
+				Scanner existingAddressName = new Scanner(System.in);
+				System.out.println("Enter name of address book: ");
+				String nameOfExistingAddressBook = existingAddressName.nextLine();
+				if (addressBookObj.containsKey(nameOfExistingAddressBook)) {
+					addressBookObj.get(nameOfExistingAddressBook);
+					AddressBookMain.entries();
+				} else {
+					System.out.println("Address book not found!!");
+				}
+			case 3:
+				entry = 3;
+				break;
+			default:
+				System.out.println(" Enter valid Input");
+				break;
+			}
+		}
+	}
+
+	public static void entries() {
 		int choice;
 		System.out.println("Welcome to Address Book");
+
 		do {
 			System.out.println("1.Add Contact \n2.Edit Existing Contact \n3.Delete Contact "
-										+ "\n4.Add Multiple Contacts \n5.Exit");
-			Scanner sc = new Scanner(System.in);
+					+ "\n4.Add Multiple Contacts \n5.Exit");
 			choice = sc.nextInt();
 			switch (choice) {
 			case 1:
@@ -34,12 +78,17 @@ public class AddressBookMain {
 				addressBook.addMultipleContacts();
 				break;
 			case 5:
-				System.out.println("Exited Successfully!");
+				System.out.println("Exited to main menu!!");
+				addAddressBook();
 				break;
 			default:
 				System.out.println("Choose correct option from above mentioned option only!!");
 				break;
 			}
 		} while (choice != 5);
+	}
+
+	public static void main(String[] args) {
+		addAddressBook();
 	}
 }
