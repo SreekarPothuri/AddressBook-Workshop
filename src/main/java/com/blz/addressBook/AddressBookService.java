f3f221bec2581078eed6bde8b3420e90a7583f2c
Your Name <you@example.com>
2020-12-03 17:34:01 +0530

    Added: editContact method to edit and modify existing contact details from contact list

--- src/main/java/com/blz/addressBook/AddressBookService.java
+++ src/main/java/com/blz/addressBook/AddressBookService.java
@@ -8,7 +8,7 @@ public class AddressBookService {
 
 	Scanner sc = new Scanner(System.in);
 	List<ContactDetails> contactList = new ArrayList<ContactDetails>();
-	
+
 	public void addContact() {
 		System.out.println("Enter First Name: ");
 		String firstName = sc.nextLine();
@@ -27,9 +27,71 @@ public class AddressBookService {
 		sc.nextLine();
 		System.out.println("Enter your emailId: ");
 		String email = sc.nextLine();
-		
+
 		ContactDetails contact = new ContactDetails(firstName, lastName, address, city, state, zip, phoneNum, email);
 		contactList.add(contact);
+		System.out.println(contactList);
+	}
+
+	public void editContact() {
+		System.out.println("Enter First name of contact that you want to edit: ");
+		String firstNameToEdit = sc.nextLine();
+		for (int id = 0; id < contactList.size(); id++) {
+			if (contactList.get(id).getFirstName().equals(firstNameToEdit)) {
+				System.out.println("Index: " + contactList.get(id));
+				Scanner editInput = new Scanner(System.in);
+				System.out.println("Enter your choice: \n1.First Name \n2.Last Name \n3.Address \n4.City \n5.State "
+						+ "\n6.Zip Code \n7.Phone Number \n8.EmailId");
+				int choice = sc.nextInt();
+				switch (choice) {
+				case 1:
+					System.out.println("Enter new First Name: ");
+					String newFirstName = editInput.nextLine();
+					contactList.get(id).setFirstName(newFirstName);
+					break;
+				case 2:
+					System.out.println("Enter new Last Name: ");
+					String newLastName = editInput.nextLine();
+					contactList.get(id).setLastName(newLastName);
+					break;
+				case 3:
+					System.out.println("Enter new Address: ");
+					String newAddress = editInput.nextLine();
+					contactList.get(id).setAddress(newAddress);
+					break;
+				case 4:
+					System.out.println("Enter new City: ");
+					String newCity = editInput.nextLine();
+					contactList.get(id).setCity(newCity);
+					break;
+				case 5:
+					System.out.println("Enter new State: ");
+					String newState = editInput.nextLine();
+					contactList.get(id).setState(newState);
+					break;
+				case 6:
+					System.out.println("Enter new ZipCode: ");
+					int newZip = editInput.nextInt();
+					contactList.get(id).setZip(newZip);
+					break;
+				case 7:
+					System.out.println("Enter new Phone Number: ");
+					long newPhoneNum = editInput.nextLong();
+					contactList.get(id).setPhoneNum(newPhoneNum);
+					break;
+				case 8:
+					System.out.println("Enter new Email Id: ");
+					String newEmail = editInput.nextLine();
+					contactList.get(id).setEmail(newEmail);
+					break;
+				default:
+					System.out.println("Choose right choice!!");
+					break;
+				}
+			} else {
+				System.out.println("No contact with such name exists!!");
+			}
+		}
 		System.out.println(contactList);
 	}
 }
